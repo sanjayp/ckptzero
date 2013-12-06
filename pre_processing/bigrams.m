db44bd268610935f0e6cf4a2bb152bace6ff3e7b
Sanjay Paul <paul.sanjaynj@gmail.com>
2013-12-06 13:52:11 -0500

    Added slicing and data clearing for the bigram script.

--- pre_processing/bigrams.m
+++ pre_processing/bigrams.m
@@ -1,14 +1,19 @@
 % NOTE: Alter this input to process modified review data.
 M = load('../data/metadata.mat');
 
+% Select the total number of samples to use. For the training data,
+% the range is 1 to 25000.
+SLICE_SIZE = 100;
+
 % Map bit vectors to pairs
 data = M.train_metadata;
+clear M
 map = containers.Map();
-for i = 1:length(data)
+for i = 1:SLICE_SIZE
 	review = data(i).text;
 	fst = 1;
 	snd = 2;
-	%fprintf('%i\n',i);
+	% fprintf('%i\n',i);
 	while snd < length(review)
 		gram = char(strcat(lower(review(fst)),'_',lower(review(snd))));
 		if isKey(map,gram)
@@ -16,7 +21,7 @@ for i = 1:length(data)
 			vect(1,i) = 1;
 			map(gram) = vect;
 		else
-			map(gram) = zeros(1,length(data));
+			map(gram) = zeros(1,SLICE_SIZE);
 		end
 		fst = fst + 1;
 		snd = snd + 1;
@@ -24,7 +29,9 @@ for i = 1:length(data)
 end
 
 % Construct final matrix
+clear data
 valueset = values(map);
+clear map
 mat = sparse(vertcat(valueset{:})');
 
 save '../data/bigram.mat' mat
